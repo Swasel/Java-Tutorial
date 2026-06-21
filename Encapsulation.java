a729077c49d76f9086d170276592bd00f5976252
Swathy <swathyselva2205@gmail.com>
2026-06-21 10:52:07 +0530

    Encapsulation

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,8 +1,20 @@
 class Human {
 
-  // 1. Private variables hide the data from outside classes
+  // Private variables hide the data from outside classes
   private int age;
   private String name;
+
+  // default constructor
+  public Human() {
+    age = 18;
+    name = "Dean";
+  }
+
+  // parameterised constructor
+  public Human(int age, String name) {
+    this.age = age;
+    this.name = name;
+  }
 
   public int getAge() {
     return age;
@@ -24,8 +36,11 @@ class Human {
 public class Encapsulation {
   public static void main(String[] args) {
     Human obj = new Human();
+    Human obj1 = new Human(27, "Logan");
     obj.setAge(22);
     obj.setName("Swathy");
     System.out.println(obj.getName() + " " + obj.getAge());
+    System.out.println(obj1.getName() + " " + obj1.getAge());
+
   }
 }
